d780aa6c7f319813abf74a7db31dc21750fb3ffd
Coder Black <165771215+Rayflex189@users.noreply.github.com>
2025-03-21 21:57:31 +0100

    Update settings.py
    Save

--- Axis/Axis/settings.py
+++ Axis/Axis/settings.py
@@ -30,7 +30,9 @@ DATABASES = {
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
+SUPERUSER_USERNAME = "admin"
+SUPERUSER_EMAIL = "admin@example.com"
+SUPERUSER_PASSWORD = "adminpassword"
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
